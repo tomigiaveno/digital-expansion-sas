38f76d3aa9f1e565f02a85873940cd01ba62bc45
tomigiaveno <119363080+tomigiaveno@users.noreply.github.com>
2026-06-22 12:23:35 -0300

    Add files via upload

---
+++ DIGITAL EXPANSION SAS/script/script.js
@@ -0,0 +1,14 @@
+const productos = [
+{
+nombre:"IBM Power E1150",
+categoria:"Servidor"
+},
+{
+nombre:"IBM FlashSystem 9500",
+categoria:"Storage"
+},
+{
+nombre:"IBM Storage Protect",
+categoria:"Software"
+}
+];
